1389ef8b837a89f27b7b24fd129d89f30cf24716
Anh Quach (Tony) <anh.quachquituan@halliburton.com>
2021-10-15 19:26:20 +0700

    Add outputs

--- outputs.tf
+++ outputs.tf
@@ -13,6 +13,21 @@ output "vpc_cidr_block" {
   value       = aws_vpc.vpc.cidr_block
 }
 
+output "default_network_acl_id" {
+  description = "The ID of the default network ACL"
+  value       = aws_vpc.vpc.default_network_acl_id
+}
+
+output "default_route_table_id" {
+  description = "The ID of the default route table"
+  value       = aws_vpc.vpc.default_route_table_id
+}
+
+output "default_security_group_id" {
+  description = "The ID of the security group created by default on VPC creation"
+  value       = aws_vpc.vpc.default_security_group_id
+}
+
 output "private_subnets" {
   description = "List of IDs of private subnets"
   value       = aws_subnet.private.*.id
