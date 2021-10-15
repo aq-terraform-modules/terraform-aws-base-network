d9d0b1e6551e804ad8e919c5adc8e3b866d3e5ad
Anh Quach (Tony) <anh.quachquituan@halliburton.com>
2021-10-15 17:20:53 +0700

    Fix error

--- main.tf
+++ main.tf
@@ -95,7 +95,7 @@ resource "aws_nat_gateway" "nat" {
   count = var.enable_nat_gateway ? local.nat_gateway_count : 0
 
   allocation_id = element(aws_eip.nat.*.id, count.index)
-  subnet_id     = element(aaws_subnet.public.*.id, count.index)
+  subnet_id     = element(aws_subnet.public.*.id, count.index)
 
   depends_on = [aws_internet_gateway.igw]
 }
